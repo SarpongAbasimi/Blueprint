e3a22e26c68a1854471ce0db86feb60099fe47c3
sarpong <abasimisarpong@rocketmail.com>
2019-07-06 20:49:51 +0100

    refactored test my using response fixtures in test modules

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,17 +1,19 @@
 import pytest
 
+@pytest.fixture(autouse=True)
+def response(client):
+  response = client.get('/')
+  return response
+
 class TestMainRoutes():
 
-  def test_index_page(self, client):
-    response = client.get('/')
+  def test_index_page(self, response):
     assert response.status_code == 200
-
-  def test_index_content_type(self, client):
-    response = client.get('/')
+  
+  def test_index_content_type(self, response):
     assert response.content_type == 'text/html; charset=utf-8'
 
-  def test_index_page_content(self, client):
-    response = client.get('/')
+  def test_index_page_content(self, response):
     assert b'Welcome To The Home Page' in response.data
   
   # @pytest.mark.parametrize('data', [('I need to go shopping'), ('I have to do my homework')])
