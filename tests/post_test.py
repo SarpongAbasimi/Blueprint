4cd9478fed7c61cd808d0aa09191fc8cc107a2ed
sarpong <abasimisarpong@rocketmail.com>
2019-07-07 19:01:59 +0100

    skipped db test to structure application config

--- tests/post_test.py
+++ tests/post_test.py
@@ -17,3 +17,10 @@ class TestPost(object):
   def test_post_new_allow_user_to_post_toDo_(self, client):
     post_message = client.post('/create', data=('I need to go shopping'),follow_redirects=True)
     assert b'Welcome To The Home Page' in  post_message.data
+
+  @pytest.mark.skip(reason='I need a better understanding of how to set up a config file')
+  def test_post_is_stored_shown_on_index_page(self, client):
+    post_message = client.post('/create', data=('I need to go shopping'),follow_redirects=True)
+    assert b'I need to go shopping' in post_message.data
+  
+  
