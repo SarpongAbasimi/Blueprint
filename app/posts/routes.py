5f0674222e6cd1999de549e5d5cae298b8ed0b98
sarpong <abasimisarpong@rocketmail.com>
2019-07-07 12:11:46 +0100

    added a new create route to create post

--- app/posts/routes.py
+++ app/posts/routes.py
@@ -1,10 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+from .forms import PostForm
 
 post = Blueprint('post', __name__)
 
 @post.route('/new', methods=['GET'])
 def new():
-  return render_template('new.html')
+  post_form = PostForm()
+  return render_template('new.html', post_form = post_form)
+
+@post.route('/create', methods=['POST'])
+def create():
+  post_form = PostForm()
+  return redirect(url_for('main.index'))
+
+
 
 
 
