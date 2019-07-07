210dfa56d1e8d39f536a72c76a28127475e3296e
sarpong <abasimisarpong@rocketmail.com>
2019-07-07 12:10:55 +0100

    added a secret key for csrf token

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask
 from app.main.routes import main
 from app.posts.routes import post
 
 def current_app():
   app = Flask(__name__)
+  app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'mykey'
 
   app.register_blueprint(main)
   app.register_blueprint(post)
