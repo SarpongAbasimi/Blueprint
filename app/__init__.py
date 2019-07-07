a87369a5cbc2f60a0ef94aa32da2b50b423a7beb
sarpong <abasimisarpong@rocketmail.com>
2019-07-07 19:00:12 +0100

    moved secret key into an env variable

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from app.main.routes import main
 from app.posts.routes import post
 
 def current_app():
   app = Flask(__name__)
   app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'mykey'
+
 
   app.register_blueprint(main)
   app.register_blueprint(post)
