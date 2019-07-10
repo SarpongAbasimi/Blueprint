a3867d86a21168e242a7c0e8c9bc17c9e71672fa
sarpong <abasimisarpong@rocketmail.com>
2019-07-10 11:04:35 +0100

    imported the .env file before the Config class is created

--- app/config.py
+++ app/config.py
@@ -1,7 +1,8 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+base_dir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(base_dir, '.env'))
 
 class Config:
   DEBUG = os.getenv('DEBUG_STATE')
