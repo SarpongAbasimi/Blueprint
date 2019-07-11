991683c0bc0a6fc6df70fe9fab181713162bf80a
sarpong <abasimisarpong@rocketmail.com>
2019-07-11 17:18:41 +0100

    added a dogstring to explain test_env

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
 import pytest
 from app import create_app, db
+from app.models import Todo
 
 @pytest.fixture()
 def client():
+  """ set the app to use testing environemnt """
   test_env = create_app('testing')
 
   test_env.app_context().push()
