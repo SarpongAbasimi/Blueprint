61e4355627a78e6ad768bf6ae1b3a134dcdfbd9f
sarpong <abasimisarpong@rocketmail.com>
2019-07-19 15:51:43 +0100

    added boostrap to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 from app.config import configs
 
 db = SQLAlchemy()
+boostrap = Bootstrap()
 
 def create_app(config_name='development'):
   app = Flask(__name__)
   app.config.from_object(configs.get(config_name))
   
   db.init_app(app)
+  boostrap.init_app(app)
 
   from app.main.routes import main
   from app.posts.routes import post
